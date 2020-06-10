938c946718fdc2c39cc7d89ec522d4b1cafa915b
yudhapratama <kartikayudhapratama@gmail.com>
2020-06-11 04:55:35 +0700

    [PARTIAL] pricing and vouchers migrations

---
+++ backend/src/database/migrations/2020_06_08_065223_create_pricing_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePricingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pricing_plans', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('name', 150);
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('discounted_price');
+            $table->boolean('status'); // true if active and false if inactive
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pricing_plans');
+    }
+}
---
+++ backend/src/database/migrations/2020_06_08_132222_create_vouchers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVouchersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',100);
+            $table->text('description');
+            $table->char('code',10);
+            $table->integer('available_quota');
+            $table->integer('used_quota');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status',['active','inactive']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+}
